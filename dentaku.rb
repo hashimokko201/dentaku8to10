3f48c115751107ece3528ea5424f583975fe90cb
hashimokko201 <marunimokko@hotmail.com>
2022-09-06 20:55:50 +0900

    first commit

---
+++ dentaku.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+
+set :environment, :production
+
+get '/' do
+  erb :index
+end
+
+post '/push' do
+str = params[:num].split("")
+int = []
+for n in str
+  int.push(n.to_i)
+end
+#puts int
+
+nod = int.length
+
+int2 = []
+
+int.each do |i|
+  int2.push(i*(8**(nod-1)))
+  nod = nod-1
+end
+
+"#{int2.sum}
+<br>
+<br>
+<a href='/'>Back</a>"
+
+end
